8bac2cc0179a45cf7e36c308ee62da636210f38c
unknown <iankwiotek@comcast.net>
2019-04-11 00:35:39 -0400

    Closing in on completion

--- posSystem/ProduceItem.cpp
+++ posSystem/ProduceItem.cpp
@@ -1,5 +1,7 @@
 #include "stdafx.h"
 #include "ProduceItem.h"
+#include <time.h>
+#include <stdio.h>
 
 ProduceItem::ProduceItem()
 {
@@ -8,15 +10,52 @@ ProduceItem::ProduceItem()
 	setPrice(00.00);
 	setQuantity(0);
 	setCountryOfOrigin("Invalid");
+	setYear(0);
+	setMonth(0);
+	setDay(0);
 }
 
-ProduceItem::ProduceItem(string name, string category, float price, int quantity, string countryOfOrigin)
+ProduceItem::ProduceItem(string name, string category, float price, int quantity, string countryOfOrigin, int year, int month, int day)
 {
 	setName(name);
 	setCategory(category);
 	setPrice(price);
 	setQuantity(quantity);
 	setCountryOfOrigin(countryOfOrigin);
+	setYear(year);
+	setMonth(month);
+	setDay(day);
+}
+
+
+void ProduceItem::setYear(int year)
+{
+	this->year = year;
+}
+
+void ProduceItem::setMonth(int month)
+{
+	this->month = month;
+}
+
+void ProduceItem::setDay(int day)
+{
+	this->day = day;
+}
+
+int ProduceItem::getYear()
+{
+	return year;
+}
+
+int ProduceItem::getMonth()
+{
+	return month;
+}
+
+int ProduceItem::getDay()
+{
+	return day;
 }
 
 void ProduceItem::setCountryOfOrigin(string countryOfOrigin)
@@ -28,3 +67,12 @@ string ProduceItem::getCountryOfOrigin()
 {
 	return countryOfOrigin;
 }
+
+//ProduceItem::ProduceItem(string name, string category, float price, int quantity, string countryOfOrigin)
+//{
+//	setName(name);
+//	setCategory(category);
+//	setPrice(price);
+//	setQuantity(quantity);
+//	setCountryOfOrigin(countryOfOrigin);
+//}
--- posSystem/ProduceItem.h
+++ posSystem/ProduceItem.h
@@ -3,7 +3,7 @@
 #include <iostream>
 #include <iomanip>
 #include <string>
-//#include <time.h>
+#include <time.h>
 //#include <ctime>
 #include "stdafx.h"
 #include "BaseItem.h"
@@ -14,14 +14,25 @@ class ProduceItem : public BaseItem
 {
 public:
 	ProduceItem();
-	ProduceItem(string name, string category, float price, int quantity, string countryOfOrigin);
+	ProduceItem(string name, string category, float price, int quantity, string countryOfOrigin, int year, int month, int day);
 
 	void setCountryOfOrigin(string countryOfOrigin);
+	void setYear(int year);
+	void setMonth(int month);
+	void setDay(int day);
 
+	int getYear();
+	int getMonth();
+	int getDay();
 	string getCountryOfOrigin();
 
 private:
 	string countryOfOrigin;
+	int year;
+	int month;
+	int day;
 };
 
 #endif 
+
+/*ProduceItem(string name, string category, float price, int quantity, string countryOfOrigin);*/
